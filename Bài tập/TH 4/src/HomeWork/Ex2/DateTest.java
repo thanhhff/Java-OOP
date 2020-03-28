5faa837e92821657761264b2e4a9bbc0f5b7fb36
Thanhhff <thanhnguyen200399@gmail.com>
2020-03-28 14:26:59 +0700

    Rename TestMyDate to DateTest

--- Bài tập/TH 4/src/HomeWork/Ex2/DateTest.java
+++ Bài tập/TH 4/src/HomeWork/Ex2/DateTest.java
@@ -5,7 +5,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class TestMyDate {
+public class DateTest {
 
     public static void main(String[] args) {
         MyDate obj1 = new MyDate();
